8cbd67cc51b54a50ab03fa46d9b3e266c9c49852
thefreshduke <scottyshaw1@gmail.com>
2016-04-02 01:11:40 -0400

    Connected IBOutlets and IBActions to GameViewController.swift

--- PearlHacks2016RockPaperScissors/GameViewController.swift
+++ PearlHacks2016RockPaperScissors/GameViewController.swift
@@ -6,4 +6,40 @@
 //  Copyright © 2016 ___sks6___. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class GameViewController: UIViewController {
+    
+    var playerChoice: Int = 0
+    
+    var computerChoice: Int = 0
+    
+    @IBOutlet weak var playerChoiceLabel: UILabel!
+    
+    @IBOutlet weak var computerChoiceLabel: UILabel!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func activatePaper(sender: AnyObject) {
+        playerChoice = 2
+    }
+    
+    @IBAction func activateRock(sender: AnyObject) {
+        playerChoice = 1
+    }
+    
+    @IBAction func activateScissors(sender: AnyObject) {
+        playerChoice = 3
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
