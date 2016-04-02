a4fddde1a2b862d6211c96f2f466bf553cd54f5d
thefreshduke <scottyshaw1@gmail.com>
2016-04-02 00:02:46 -0400

    Created layout; added buttons and labels; connected view controllers

---
+++ PearlHacks2016RockPaperScissors/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PearlHacks2016RockPaperScissors
+//
+//  Created by Scotty Shaw on 4/1/16.
+//  Copyright © 2016 ___sks6___. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
